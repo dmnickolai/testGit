aaa507b43b74c59e8df99ca05b3134a03018b042
Dennis <dmnickolai@gmail.com>
2020-08-19 06:34:52 -0700

    Initial Save

---
+++ src/testsqlite/DummyClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testsqlite;
+
+/**
+ *
+ * @author Dennis
+ */
+public class DummyClass {
+    // new class for Git
+    //add it Git
+}
